bbf3b1ee38499854a8802556749e42fdd15238e6
chris <dwatow@gmail.com>
2022-12-03 00:49:12 +0800

    modify host

--- src/utility/jenkinsAPI/index.js
+++ src/utility/jenkinsAPI/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const jenkinsAPI = axios.create({
-  baseURL: `https://my-god.herokuapp.com`,
+  baseURL: `https://my-god-render.onrender.com`,
 });
 
 async function fetchUpdateStatus(params) {
