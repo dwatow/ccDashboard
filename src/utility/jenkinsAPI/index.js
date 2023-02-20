afb61af528f06de1e6691a98bd1d75f51954ab6d
Chris <dwatow@gmail.com>
2023-02-20 11:44:13 +0800

    Update index.js

--- src/utility/jenkinsAPI/index.js
+++ src/utility/jenkinsAPI/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const jenkinsAPI = axios.create({
-  baseURL: `my-god.cyclic.app`,
+  baseURL: `https://my-god.cyclic.app`,
   // baseURL: `http://localhost:3030`,
 });
 
